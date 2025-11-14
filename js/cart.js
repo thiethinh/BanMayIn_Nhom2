dc13d0b3d350af40756b2bcf6542b4433e359d7d
ThaiNguyenIT <23130296@st.hcmuaf.edu.vn>
2025-11-14 18:43:47 +0700

    fix link + display  all Page

--- js/cart.js
+++ js/cart.js
@@ -2,7 +2,7 @@
 let products = [
     {id: 1, name: "Máy in phun màu Epson L6270", price: 16990000 , quantity: 1},
     {id: 2, name: "Máy in HP Laser MFP 3303fdn", price: 10490000, quantity: 2},
-    {id: 3, name: "Máy in Laser DCP-L3560CDW", price: 11790000, quantity: 1}
+    // {id: 3, name: "Máy in Laser DCP-L3560CDW", price: 11790000, quantity: 1}
     // {id: 4; name: "Máy in Brother HL-L2321D", price: 3000000, quantity: 1},
     // {id: 5; name: "Máy in Samsung Xpress M2020W", price: 2800000, quantity: 1}
 ];
